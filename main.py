084cfb33866b1e17fc3ed3912db7267fe5ad0803
Sebastião Figueiredo Mendes Júnior <juniorfigueiredo2007@gmail.com>
2025-03-28 19:10:35 -0300

    primeiro envio do bot

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import PlainTextResponse
+
+app = FastAPI()
+
+@app.post("/webhook")
+async def receber_mensagem(request: Request):
+    form = await request.form()
+    from_number = form.get("From") or form.get("from")
+    body = form.get("Body") or form.get("body")
+
+    print(f"Mensagem recebida de {from_number}: {body}")
+    return PlainTextResponse("Recebido com sucesso!")
